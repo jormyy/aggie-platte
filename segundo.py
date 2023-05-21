6f122abbe5879e9a4ddd1f17aba126657f72de66
Jeremy Ha <jorm@Jeremys-MacBook-Pro-3.local>
2023-05-20 22:26:02 -0700

    organized food items

--- segundo.py
+++ segundo.py
@@ -44,10 +44,35 @@ def Dinner(todays):
         if "Dinner" in meal.text:
             return meal
 
+def Items(the_meal):
+    items = []
+    possible_items = the_meal.find_all("li", class_ = "trigger")
+    for food_item in possible_items:
+        if "N/A" not in food_item.text:
+            items.append(food_item)
+    
+    return items
+
+def NutritionFacts(menu_items):
+    food_dictionary = {}
+    for item in menu_items:
+        name = item.find("span", class_ = "").text.strip()
+        nutrition_divs = item.find_all("div", class_ = "")
+        for div_item in nutrition_divs:
+            if div_item.find("h6", class_ = "") == None:
+                continue
+            nutrient_items = div_item.find_all("h6", class_ = "")
+            nutrient_values = div_item.find_all("p", class_ = "")
+            for (nut_index, nut_value) in zip(nutrient_items, nutrient_values):
+                if "Allergens" in nut_index.text:
+                    break
+                food_dictionary[name + " " + nut_index.text.strip()] = nut_value.text.strip()
+    
+    return food_dictionary
+
 day_index = IV.WhatDay()
 today_menu = TodayMenu(day_index, soup)
 this_meal = Meal(today_menu)
-print(this_meal)
-# menu_items = today_menu.find_all("li", class_ = "trigger")
-# for item in menu_items:
-#     print(item.find("span", class_ = "").text.strip())
+menu_items = Items(this_meal)
+nutrition_facts = NutritionFacts(menu_items)
+print(nutrition_facts)
